eaa6352d812eb8d65c0861ad9360566355bd7294
YanaSolo <cammeonn@gmail.com>
2025-02-19 07:21:50 +0300

    0.0.4

--- password/PasswordGeneration.py
+++
@@ -1,9 +0,0 @@
-from random import *
-
-
-def Generation():
-    res = ''
-    for i in range(randrange(5, 17)):
-        alf = '1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
-        res += alf[randrange(1, len(alf))]
-    return res
