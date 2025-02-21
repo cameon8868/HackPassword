6b802e9b2274302cf56adbf30a0b398ab15e3cf5
YanaSolo <cammeonn@gmail.com>
2025-02-21 08:44:21 +0300

    0.0.6

--- main.py
+++ main.py
@@ -1,12 +1,39 @@
 from tkinter import *
+from tkinter.scrolledtext import ScrolledText
 from PasswordBruteForce import *
 from PasswordGeneration import *
 
+a = []
+def show_message():
+    label['text'] = 'Введите заметку'
+    if entry.get() != '': 
+        a.append(f'{len(a) + 1}.' + Generation() + f' - {entry.get()}')
+        res = ''
+        for i in a:
+            res += '' + i + '\n'
+        
+        st.delete('1.0', END)
+        st.insert(END, f'{res}') 
+        label['text'] = res
+
+
+    
 
 root = Tk()
-root.title('PasswordHack')
-root.geometry('300x200')
-Entry().pack(anchor=NW, padx=150, pady= 100)
+root.title('PasswordHack(YanaSola)')
+root.geometry('400x300')
+	
+st = ScrolledText(root, width=50,  height=10)
+st.pack(fill=BOTH, side=LEFT, expand=True)
 
+entry = Entry()
+entry.pack(anchor=NW, padx=6, pady=6)
+  
+btn = Button(text="Click", command=show_message)
+btn.pack(anchor=NW, padx=6, pady=6)
 
+label = Label()
+label.pack(anchor=NW, padx=100, pady=6)
+
+  
 root.mainloop()
